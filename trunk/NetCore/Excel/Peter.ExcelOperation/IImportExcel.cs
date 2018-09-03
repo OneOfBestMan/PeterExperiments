09f6f568bc4b32aae0f7951947c0bc66aa0dbb07
peter.liu <1065114664@qq.com>
2018-09-03 17:56:33 +0800

    Excel Import

---
+++ trunk/NetCore/Excel/Peter.ExcelOperation/IImportExcel.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Http;
+using OfficeOpenXml;
+using System.Collections.Generic;
+using System.Data;
+using System.Threading.Tasks;
+
+namespace Peter.ExcelOperation
+{
+	public interface IImportExcel
+	{
+
+		Task<DataTable> Upload(IFormFile file);
+		Task<ImportResult<T>> Upload<T>(IFormFile file) where T : class;
+		ImportResult<T> Valid<T>(ExcelWorksheet ws) where T : class;
+		string MakeSql(ExcelWorksheet worksheet);
+		bool Execute(string sql);
+		ValidOption Options { get; set; }
+	}
+
+	public class ImportResult<T> where T : class
+	{
+		public List<ErrorMessage> Messages { get; set; }
+		public List<T> Data { get; set; }
+	}
+
+	public class ErrorMessage
+	{
+		public string Name { get; set; }
+		public string Nums { get; set; }
+		public string Positions { get; set; }
+	}
+}
---
+++ trunk/NetCore/Excel/Peter.ExcelOperation/ValidOption.cs
@@ -0,0 +1,17 @@
+﻿using OfficeOpenXml;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Peter.ExcelOperation
+{
+	public class ValidOption
+	{
+		/// <summary>
+		/// 验证列数
+		/// </summary>
+		public int ColumnsShouldBe { get; set; }
+
+
+	}
+}
