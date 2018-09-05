fb9af0ff427ae3698a34e66b4f4bd958fc457a16
peter liu <1065114664@qq.com>
2018-09-05 23:29:42 +0800

    course splider 33

--- trunk/Python/SpiderFramework/TestGetUrls.py
+++ trunk/Python/SpiderFramework/TestGetUrls.py
@@ -2,6 +2,8 @@
 import TaskOption
 import CockieProvider
 import Manager
+import BaseRequest
+
 
 class TestGetUrls(object):
       
@@ -9,18 +11,41 @@ class TestGetUrls(object):
                  'Accept': "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8",
                  'Accept - Encoding': "gzip, deflate, br",
                  'Connection': "Keep-Alive",
-                 'User-Agent': self.user_agent,
+                 'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/68.0.3440.106 Safari/537.36'
                  #'Cookie': self.cookies
                }
-      option = TaskOption("https://www.youngmaker.com/", "https://www.youngmaker.com/",'https://www.youngmaker.com/home/courses/study/capid/7255/catid/292/sectionid/2100/classroomid/29/ccid/14.html',"mp4", 5, headers,"")
+      option = TaskOption.TaskOption("https://www.youngmaker.com/", "https://www.youngmaker.com/",'https://www.youngmaker.com/home/courses/study/capid/7255/catid/292/sectionid/2100/classroomid/29/ccid/14.html',"mp4", 5, headers,"")
       cookie_dict=dict()
-      manager=Manager()
+      manager=Manager.Manager()
 
-      def startSplider():
+      def startSplider(self):
+          level=1
           login_name = '13501884996'
           login_password = 'youngmaker22'
-          provider=CockieProvider(option)
+          provider=CockieProvider.CockieProvider(TestGetUrls.option)
           cookie_dict=provider.login(login_name,login_password)
+          self.getAllUrls(TestGetUrls.option.beginUrl,1,cookie_dict)
+          for dto in TestGetUrls.manager.Urls:
+              print(dto.name+":"+dto.url)
+          #files=request.downloadUrlsFromPage(html,cookie_dict)
+
+      def getAllUrls(self,baseUrl,level,cookie):
+          print("获取url"+baseUrl+',level='+str(level))
+          request=BaseRequest.BaseRequest(TestGetUrls.option)
+          html=request.getPage(baseUrl,cookie)
+          urls=request.getSearchUrlsFromPage(html)
+          dtos=[]
+          if len(urls) >0  and level< 5:
+             for key in urls.keys():
+                 if "course" in urls[key] or "Course" in urls[key]: 
+                     dto=Manager.UrlDto(key,urls[key],level,False)
+                     dtos.append(dto)
+             TestGetUrls.manager.AddUrls(dtos)
+             for key in urls.keys():
+                 if "course" in urls[key] or "Course" in urls[key]:
+                     self.getAllUrls(urls[key],level+1,cookie)
+
+
 
 
 
